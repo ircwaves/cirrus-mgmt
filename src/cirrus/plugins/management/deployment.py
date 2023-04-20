f115c93bbd65f60ce2b820e5d4373fc4057cff0a
Arthur Elmes <aelmes@element84.com>
2023-04-20 14:09:39 -0400

    add accessor to exception

--- src/cirrus/plugins/management/deployment.py
+++ src/cirrus/plugins/management/deployment.py
@@ -213,7 +213,9 @@ class Deployment(DeploymentMeta):
             session=self.get_session(),
         )
 
-        @backoff.on_exception(backoff.expo, PayloadNotFoundError, max_time=60)
+        @backoff.on_exception(
+            backoff.expo, exceptions.PayloadNotFoundError, max_time=60
+        )
         def _get_payload_item_from_statedb(statedb, payload_id):
             return statedb.get_dbitem(payload_id)
 
